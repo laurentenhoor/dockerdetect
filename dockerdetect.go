748020598d1ad785cc01a1708e85973cb6917acf
Lauren ten Hoor <laurentenhoor@gmail.com>
2019-03-10 23:56:24 +0100

    Rewritten to importable library

--- dockerdetect.go
+++ dockerdetect.go
@@ -10,38 +10,37 @@ import (
 	"runtime"
 )
 
-// DockerResponse is an API response
-type DockerResponse struct {
+type dockerResponse struct {
 	Installed               bool
 	InstallationURL         string
 	InstallationInstruction string
 }
 
-func init() {
+// InitAPI creates the docker detection API endpoint
+func InitAPI() {
 	router := mux.NewRouter()
-	router.HandleFunc("/docker", CheckDocker).Methods("GET")
+	router.HandleFunc("/dockerdetect", checkDocker).Methods("GET")
 	log.Fatal(http.ListenAndServe("localhost:8000", router))
 }
 
-// CheckDocker will return whether Docker is installed, furthermore it will give installation relevant installation instructions
-func CheckDocker(w http.ResponseWriter, r *http.Request) {
-	var response DockerResponse
+func checkDocker(w http.ResponseWriter, r *http.Request) {
+	var response dockerResponse
 
 	if dockerIsNotInstalled() {
 		if runtime.GOOS == "windows" {
-			response = DockerResponse{Installed: false, InstallationURL: "https://hub.docker.com/editions/community/docker-ce-desktop-windows"}
+			response = dockerResponse{Installed: false, InstallationURL: "https://hub.docker.com/editions/community/docker-ce-desktop-windows"}
 
 		} else if runtime.GOOS == "darwin" {
-			response = DockerResponse{Installed: false, InstallationURL: "https://hub.docker.com/editions/community/docker-ce-desktop-mac"}
+			response = dockerResponse{Installed: false, InstallationURL: "https://hub.docker.com/editions/community/docker-ce-desktop-mac"}
 
 		} else {
-			response = DockerResponse{Installed: false, InstallationInstruction: "Installation script for Linux started in the background."}
+			response = dockerResponse{Installed: false, InstallationInstruction: "Installation script for Linux started in the background."}
 			exec.Command("curl -fsSL https://get.docker.com -o get-docker.sh")
 			exec.Command("sudo sh get-docker.sh")
 
 		}
 	} else {
-		response = DockerResponse{Installed: true, InstallationInstruction: "Docker is already installed."}
+		response = dockerResponse{Installed: true, InstallationInstruction: "Docker is already installed."}
 	}
 
 	json.NewEncoder(w).Encode(response)
